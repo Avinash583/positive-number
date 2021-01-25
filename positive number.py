d8ea9555bad91e47262d1dd642ac183dc4495f1f
Avinash583 <74305885+Avinash583@users.noreply.github.com>
2021-01-25 20:29:26 +0530

    Add files via upload

---
+++ positive number.py
@@ -0,0 +1,8 @@
+list1 = [12,-7,5,64,-14]
+for number in list1:
+    if number >= 0:
+        print(number,end="")
+        list2 = [-4,-9,5]
+        for num in list2:
+            if num >= 0:
+                print(num)
